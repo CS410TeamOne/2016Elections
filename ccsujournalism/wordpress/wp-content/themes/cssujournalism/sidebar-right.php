508ce49b3ef47329cddc51214f9d237a2dbcd897
jokelly106 <johnokelly106@gmail.com>
2015-11-23 23:33:44 -0500

    Enhanced sidebars, fixed accidentally deleting a bunch of important stuff
    Sidebars and main div now flow together properly and look pretty decent.
    Should be in good shape for meeting tomorrow.

--- ccsujournalism/wordpress/wp-content/themes/cssujournalism/sidebar-right.php
+++ ccsujournalism/wordpress/wp-content/themes/cssujournalism/sidebar-right.php
@@ -1,3 +1,12 @@
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.5&appId=239223902795699";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="fb-page" data-href="https://www.facebook.com/CcsuJournalism/?fref=nf" data-small-header="true" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="false" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/CcsuJournalism/?fref=nf"><a href="https://www.facebook.com/CcsuJournalism/?fref=nf">CCSU Journalism</a></blockquote></div></div>
+<hr/>
 <div class="panel panel-default">
             <div class="panel-heading">
                 <h3 class="panel-title"><span class="glyphicon glyphicon-ok"></span> More Quick Links</h3>
@@ -12,3 +21,4 @@
                 
             </div>
 </div>
+
