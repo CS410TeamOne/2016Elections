e76bed108e8371980d4ba4dfff552e95e16813cd
jokelly106 <johnokelly106@gmail.com>
2015-12-08 13:40:43 -0500

    actually adjusted category display

--- ccsujournalism/wordpress/wp-content/themes/cssujournalism/category.php
+++ ccsujournalism/wordpress/wp-content/themes/cssujournalism/category.php
@@ -24,15 +24,34 @@ get_header();
             <div class="page-header"><h1><span class="glyphicon glyphicon-<?php
             echo get_category_glyph($category);
             ?>"></span> <?php echo $category->name; ?></h1></div>
-            <div class="row">
+            
                     <?php if (have_posts()) : ?>
                                                    <?php while (have_posts()) : the_post(); ?>
-                        <div class="col-md-6">
-                            <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                                                   <table class="table table-striped table-condensed">
+            <tr><td>
+                            <h2><?php
+                            if (has_post_thumbnail()) {
+                                the_post_thumbnail(array(100, 100));
+                            } else {
+                                echo '<img src="' . get_template_directory_uri() . '/img/no_img.jpg"/>';
+                            }?>
+                            <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); 
+                            if (in_category("videos") || has_tag("video")){
+														echo get_glyph("video");
+													}
+													if(in_category("audio") || has_tag("audio")){
+														echo get_glyph("audio");
+													}
+													if(in_category("images") || has_tag("images") || has_tag ("image") || has_tag("gallery")){
+														echo get_glyph("gallery");
+													}?></a></h2>
+                                                    </tr></td><tr><td>
                             <small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
                             <hr/>
         <?php the_excerpt(); ?>
-                        </div>
+                        
+                        </tr></td>
+                        </table>
                         <?php endwhile; ?>
                     <?php else: ?>
                     <p>Sorry, no posts matched your criteria.</p>
