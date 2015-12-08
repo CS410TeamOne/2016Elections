32032a40afe0e7cb107ef1379c6834978349f568
steve1124 <s.lesiczka@my.ccsu.edu>
2015-12-08 15:06:26 -0500

    Added live coverage page

---
+++ ccsujournalism/wordpress/wp-content/themes/cssujournalism/page-live.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<section class="left-bar" id="sidebar_left" <?php echo $str ?>><?php dynamic_sidebar('left'); ?></section>
+        <section class="right-bar" id="sidebar_right" <?php echo $str ?>><?php dynamic_sidebar('right'); ?></section>
+        <div class="content">
+
+<iframe width="720" height="405" src="http://www.ustream.tv/embed/21655149?html5ui" allowfullscreen webkitallowfullscreen scrolling="no" frameborder="0" style="border: 0 none transparent;"></iframe>
+
+<?php get_footer(); ?>
