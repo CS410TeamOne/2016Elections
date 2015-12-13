db1178ce4ff64c0405a842c5b7f8499d04d8d2a8
jokelly106 <johnokelly106@gmail.com>
2015-12-13 17:21:59 -0500

    Added .zip for theme install, added theme only folder

---
+++ Theme Only/category-videos.php
@@ -0,0 +1,31 @@
+<?php get_page_header() ?>
+<div class="well">
+<?php
+    $get_cat = get_query_var('cat');
+    $category = get_category($get_cat);
+    ?>
+    <div class="page-header">
+        <h1>
+            <span class="glyphicon glyphicon-<?php echo get_category_glyph($category); ?>"></span> 
+            <?php echo $category->name; ?>
+        </h1>
+    </div>
+    <div class="row">
+    <?php
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            display_video_category();
+        endwhile;
+        the_posts_pagination(array(
+            'prev_text' => __('Previous page', 'ccsujournalism'),
+            'next_text' => __('Next page', 'ccsujournalism'),
+            'before_page_number' => '<span class="meta-nav screen-reader-text">' . __('Page', 'ccsujournalism') . ' </span>',
+        ));
+    else :
+        echo "No results.";
+    endif;
+    ?>
+</div>
+</div>
+</div>
+<?php get_footer(); 
